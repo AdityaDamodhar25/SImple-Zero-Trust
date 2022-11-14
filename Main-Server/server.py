770a6dcf4668ffcd1e70bc1166cd7eecab1876cd
Ubuntu <ubuntu@ip-172-31-4-119.ap-northeast-1.compute.internal>
2022-11-14 11:59:00 +0000

    Main Server Code
    Logging function completed.
    Moving to Actuator Side code

--- Main-Server/server.py
+++ Main-Server/server.py
@@ -1,9 +1,17 @@
+from csv import writer
+from csv import DictWriter
 import socket
 import json
 
 HOST = "0.0.0.0"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
+field_names = ['smoke_i', 'dis_i', 'dep_i', 'temp_i', 'hum_i', 'light_i']
+
+with open('/home/ubuntu/Logs/logs.csv','a') as f_object:
+	writer_object = writer(f_object)
+	writer_object.writerow(field_names)
+	f_object.close
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.bind((HOST, PORT))
@@ -20,3 +28,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 				D = json.loads(data.decode('utf-8'))
 				print(D)
 				conn.sendall(data)
+		print(D)
+		with open('/home/ubuntu/Logs/logs.csv','a') as f_object:
+			dictw_obj = DictWriter(f_object, fieldnames = field_names)
+			dictw_obj.writerow(D)
+			f_object.close()

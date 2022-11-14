e7fd39ea3e112e548391478aaf4a18ee6d4d3112
Ubuntu <ubuntu@ip-172-31-4-119.ap-northeast-1.compute.internal>
2022-11-14 10:29:21 +0000

    Main Server file
    Contains code to recieve input from sensor server and process the same.
    
    Yet to log the received data in files

---
+++ Main-Server/server.py
@@ -0,0 +1,22 @@
+import socket
+import json
+
+HOST = "0.0.0.0"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	s.bind((HOST, PORT))
+
+	while(True):
+		s.listen()
+		conn, addr = s.accept()
+		with conn:
+			print(f"Connected by {addr}")
+			while True:
+				data = conn.recv(1024)
+				if not data:
+					break
+				D = json.loads(data.decode('utf-8'))
+				print(D)
+				conn.sendall(data)

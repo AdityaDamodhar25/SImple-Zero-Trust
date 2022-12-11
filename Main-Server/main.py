d5035bd65b6ec32b1bdb0405fdf0187d0b799f75
Ubuntu <ubuntu@ip-172-31-4-119.ap-northeast-1.compute.internal>
2022-12-11 07:58:27 +0000

    All changes done Integrated all scripts with main.py ONly IoT left

--- Main-Server/main.py
+++ Main-Server/main.py
@@ -7,63 +7,23 @@ import rsa
 import pickle
 import time
 
+
+field_names = ['smoke_i', 'dis_i', 'dep_i', 'temp_i', 'hum_i', 'light_i']
+
+with open('/home/ubuntu/Logs/logs.csv','w') as f_object:
+    writer_object = writer(f_object)
+    writer_object.writerow(field_names)
+    f_object.close
+
 HOST = "0.0.0.0"  # Standard loopback interface address (localhost)
 PORT_S = 65432  # Port to listen on (non-privileged ports are > 1023)
 PORT_A = 54321
 
 
-Logs_Data = read_csv("/home/ubuntu/Logs/logs.csv")
-field_names = ['smoke_i', 'dis_i', 'dep_i', 'temp_i', 'hum_i', 'light_i']
-
-smoke_data = Logs_Data['smoke_i'].tolist()
-dis_data = Logs_Data['dis_i'].tolist()
-dep_data = Logs_Data['dep_i'].tolist()
-temp_data = Logs_Data['temp_i'].tolist()
-hum_data = Logs_Data['hum_i'].tolist()
-light_data = Logs_Data['light_i'].tolist()
-
-l = len(smoke_data)
-
-
-def smoke(i):
+def reading(i,log_data):
     i_ = i-1
-    abs_val = smoke_data[i_]
-    rel = smoke_data[0:i]
-    avg_val = sum(rel)/i
-    return (abs_val, avg_val)
-
-def dis(i):
-    i_ = i-1
-    abs_val = dis_data[i_]
-    rel = dis_data[0:i]
-    avg_val = sum(rel)/i
-    return (abs_val, avg_val)
-
-def dep(i):
-    i_ = i-1
-    abs_val = dep_data[i_]
-    rel = dep_data[0:i]
-    avg_val = sum(rel)/i
-    return (abs_val, avg_val)
-
-def temp(i):
-    i_ = i-1
-    abs_val = temp_data[i_]
-    rel = temp_data[0:i]
-    avg_val = sum(rel)/i
-    return (abs_val, avg_val)
-
-def hum(i):
-    i_ = i-1
-    abs_val = hum_data[i_]
-    rel = hum_data[0:i]
-    avg_val = sum(rel)/i
-    return (abs_val, avg_val)
-
-def light(i):
-    i_ = i-1
-    abs_val = light_data[i_]
-    rel = light_data[0:i]
+    abs_val = log_data[i_]
+    rel = log_data[0:i]
     avg_val = sum(rel)/i
     return (abs_val, avg_val)
 
@@ -119,6 +79,7 @@ while(True):
         except:
             print('Closed')
 
+    Logs_Data = read_csv("/home/ubuntu/Logs/logs.csv")
     s_a.listen()
     conn, addr = s_a.accept()
     with conn:
@@ -140,42 +101,43 @@ while(True):
                 print('Disconnected')
             print('Not Authenticated')
             continue
+        l = pickle.loads(conn.recv(512))
         print(f"Connected by {addr}")
-        pub_ser_2,priv_ser_2 = rsa.newkeys(1024)
-        conn.sendall(pickle.dumps(pub_ser_2))
-        pub_act_b = conn.recv(1024)
-        pub_act = pickle.loads(pub_act_b)
-        data_b = conn.recv(1024)
-        data = rsa.decrypt(data_b,priv_ser_2)
-        [param,i] = data.decode('UTF-8').split(',')
-        i = int(i)
-        (abs_val, avg_val) = (0,0)
-        if param == 'smoke':
-            (abs_val,avg_val) = smoke(i)
-            i+=1
-        elif param == 'dis':
-            (abs_val,avg_val) = dis(i)
-            i+=1
-        elif param == 'dep':
-            (abs_val,avg_val) = dep(i)
-            i+=1
-        elif param == 'temp':
-            (abs_val,avg_val) = temp(i)
-            i+=1
-        elif param == 'hum':
-            (abs_val,avg_val) = hum(i)
-            i+=1
-        elif param == 'light':
-            (abs_val,avg_val) = light(i)
-            i+=1
-        else:
-            continue
-
-        sending_data = {'Parameter':param, 'abs_val':abs_val, 'avg_val':avg_val}
-        sending_data_encoded = pickle.dumps(sending_data)
-        sending_data_enc = rsa.encrypt(sending_data_encoded, pub_act)
-        conn.sendall(sending_data_enc)
-        print("Sent Data")
+        for m in range(l):
+            pub_ser_2,priv_ser_2 = rsa.newkeys(1024)
+            conn.sendall(pickle.dumps(pub_ser_2))
+            pub_act_b = conn.recv(1024)
+            pub_act = pickle.loads(pub_act_b)
+            data_b = conn.recv(1024)
+            data = rsa.decrypt(data_b,priv_ser_2)
+            [param,i] = data.decode('UTF-8').split(',')
+            i = int(i)
+            (abs_val, avg_val) = (0,0)
+            if param == 'smoke':
+                log_data = Logs_Data['smoke_i'].tolist()
+                (abs_val,avg_val) = reading(i,log_data)
+            elif param == 'dis':
+                log_data = Logs_Data['dis_i'].tolist()
+                (abs_val,avg_val) = reading(i,log_data)
+            elif param == 'dep':
+                log_data = Logs_Data['dep_i'].tolist()
+                (abs_val,avg_val) = reading(i,log_data)
+            elif param == 'temp':
+                log_data = Logs_Data['temp_i'].tolist()
+                (abs_val,avg_val) = reading(i,log_data)
+            elif param == 'hum':
+                log_data = Logs_Data['hum_i'].tolist()
+                (abs_val,avg_val) = reading(i,log_data)
+            elif param == 'light':
+                log_data = Logs_Data['light_i'].tolist()
+                (abs_val,avg_val) = reading(i,log_data)
+            else:
+                continue
+            sending_data = {'Parameter':param, 'abs_val':abs_val, 'avg_val':avg_val}
+            sending_data_encoded = pickle.dumps(sending_data)
+            sending_data_enc = rsa.encrypt(sending_data_encoded, pub_act)
+            conn.sendall(sending_data_enc)
+            print("Sent Data")
         conn.close()
         try:
             conn.sendall(b'ping')

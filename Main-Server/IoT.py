b213a1eee779119ca654d60893334115366a9429
Ubuntu <ubuntu@ip-172-31-4-119.ap-northeast-1.compute.internal>
2022-12-21 11:09:29 +0000

    Final Push - Main Server

--- Main-Server/IoT.py
+++ Main-Server/IoT.py
@@ -7,8 +7,9 @@ import rsa
 import pickle
 import time
 
+(priv_master, pub_master) = rsa.newkeys(512)
 
-field_names = ['dis_i']
+field_names = ['dis_i','temp_i']
 
 with open('/home/ubuntu/Logs/logs.csv','w') as f_object:
     writer_object = writer(f_object)
@@ -20,12 +21,11 @@ PORT_S = 65432  # Port to listen on (non-privileged ports are > 1023)
 PORT_A = 54321
 
 
-def reading(i,log_data):
-    i_ = i-1
-    abs_val = log_data[i_]
-    rel = log_data[0:i]
-    avg_val = sum(rel)/i
-    return (abs_val, avg_val)
+def reading(ind,log_data):
+    ind_ = ind-1
+    abs_val_enc = bytes.fromhex(log_data[ind_])
+    abs_val = pickle.loads(rsa.decrypt(abs_val_enc, pub_master))
+    return (abs_val)
 
 
 
@@ -59,19 +59,33 @@ while(True):
             continue
         pub_ser_1,priv_ser_1 = rsa.newkeys(1024)
         conn.sendall(pickle.dumps(pub_ser_1))
+        print('Shared Public key to Sensor Server')
         data_enc = conn.recv(2048)
+        print('Received Data from Sensor Server')
         data = rsa.decrypt(data_enc,priv_ser_1)
         D = pickle.loads(data)
+
+        for i in field_names:
+            t = pickle.dumps(D[i])
+            t_enc = rsa.encrypt(t, priv_master)
+            D[i] = t_enc.hex()
+
+        print('Encrypted data for storage:')
+        print(D)
+
         with open('/home/ubuntu/Logs/logs.csv','a') as f_object:
             dictw_obj = DictWriter(f_object, fieldnames = field_names)
             dictw_obj.writerow(D)
             f_object.close()
+        print("Data stored in Logs")
         conn.close()
         try:
             conn.sendall(b'ping')
             print('Still open')
         except:
             print('Closed')
+
+    time.sleep(2)
 
     Logs_Data = read_csv("/home/ubuntu/Logs/logs.csv")
     s_a.listen()
@@ -96,22 +110,29 @@ while(True):
             print('Not Authenticated')
             continue
         print(f"Connected by {addr}")
-        pub_ser_2,priv_ser_2 = rsa.newkeys(1024)
-        conn.sendall(pickle.dumps(pub_ser_2))
-        pub_act_b = conn.recv(1024)
-        pub_act = pickle.loads(pub_act_b)
-        data_b = conn.recv(1024)
-        data = rsa.decrypt(data_b,priv_ser_2)
-        [param,i] = data.decode('UTF-8').split(',')
-        i = int(i)
-        (abs_val, avg_val) = (0,0)
-        log_data = Logs_Data['dis_i'].tolist()
-        (abs_val,avg_val) = reading(i,log_data)
-        sending_data = {'Parameter':param, 'abs_val':abs_val, 'avg_val':avg_val}
-        sending_data_encoded = pickle.dumps(sending_data)
-        sending_data_enc = rsa.encrypt(sending_data_encoded, pub_act)
-        conn.sendall(sending_data_enc)
-        print("Sent Data")
+        l = len(field_names)
+        for i in range(l):
+            pub_ser_2,priv_ser_2 = rsa.newkeys(1024)
+            pub_ser_2_b = pickle.dumps(pub_ser_2)
+            conn.sendall(pub_ser_2_b)
+            print('Shared Public Key',(i+1),'to Actuator Server')
+            pub_act_b = conn.recv(1024)
+            print('Received Public key', (i+1),'from Actuator Server')
+            pub_act = pickle.loads(pub_act_b)
+            data_b = conn.recv(1024)
+            data = rsa.decrypt(data_b,priv_ser_2).decode()
+            print('Received and decoded data')
+            [param,index] = data.split(',')
+            index = int(index)
+            (abs_val, avg_val) = (0,0)
+            col = field_names[i]
+            log_data = Logs_Data[col].tolist()
+            abs_val = reading(index,log_data)
+            sending_data = {'Parameter':param, 'abs_val':abs_val}
+            sending_data_encoded = pickle.dumps(sending_data)
+            sending_data_enc = rsa.encrypt(sending_data_encoded, pub_act)
+            conn.sendall(sending_data_enc)
+            print("Sent Requested Data after Encryption")
         conn.close()
         try:
             conn.sendall(b'ping')
--- Main-Server/main.py
+++ Main-Server/main.py
@@ -8,6 +8,9 @@ import pickle
 import time
 
 
+(priv_master, pub_master) = rsa.newkeys(512)
+
+
 field_names = ['smoke_i', 'dis_i', 'dep_i', 'temp_i', 'hum_i', 'light_i']
 
 with open('/home/ubuntu/Logs/logs.csv','w') as f_object:
@@ -22,10 +25,9 @@ PORT_A = 54321
 
 def reading(i,log_data):
     i_ = i-1
-    abs_val = log_data[i_]
-    rel = log_data[0:i]
-    avg_val = sum(rel)/i
-    return (abs_val, avg_val)
+    abs_val_enc = bytes.fromhex(log_data[i_])
+    abs_val = pickle.loads(rsa.decrypt(abs_val_enc, pub_master))
+    return (abs_val )
 
 
 
@@ -59,7 +61,9 @@ while(True):
             continue
         pub_ser_1,priv_ser_1 = rsa.newkeys(1024)
         conn.sendall(pickle.dumps(pub_ser_1))
+        print('Shared Main Server Public Key to Sensor Server')
         data_enc = conn.recv(2048)
+        print('Received Encrypted Data from Sensor Server')
         data = rsa.decrypt(data_enc,priv_ser_1)
         D1 = pickle.loads(data)
         data_enc = conn.recv(2048)
@@ -68,10 +72,18 @@ while(True):
 
         D = {**D1, **D2}
 
+        for i in field_names:
+            t = pickle.dumps(D[i])
+            t_enc = rsa.encrypt(t, priv_master)
+            D[i] = t_enc.hex()
+        print('Encrypted data to be stored in Logs')
+        print(D)
+
         with open('/home/ubuntu/Logs/logs.csv','a') as f_object:
             dictw_obj = DictWriter(f_object, fieldnames = field_names)
             dictw_obj.writerow(D)
             f_object.close()
+        print('Logs Written')
         conn.close()
         try:
             conn.sendall(b'ping')
@@ -106,43 +118,47 @@ while(True):
         for m in range(l):
             pub_ser_2,priv_ser_2 = rsa.newkeys(1024)
             conn.sendall(pickle.dumps(pub_ser_2))
+            print('Sent Main Server Public Key', (m+1))
             pub_act_b = conn.recv(1024)
+            print('Received Actuator Server Public Key', (m+1))
             pub_act = pickle.loads(pub_act_b)
             data_b = conn.recv(1024)
+            print('Received Encrypted Request for Data')
             data = rsa.decrypt(data_b,priv_ser_2)
+            print('Request:', data)
             [param,i] = data.decode('UTF-8').split(',')
             i = int(i)
             (abs_val, avg_val) = (0,0)
             if param == 'smoke':
                 log_data = Logs_Data['smoke_i'].tolist()
-                (abs_val,avg_val) = reading(i,log_data)
+                (abs_val) = reading(i,log_data)
             elif param == 'dis':
                 log_data = Logs_Data['dis_i'].tolist()
-                (abs_val,avg_val) = reading(i,log_data)
+                (abs_val) = reading(i,log_data)
             elif param == 'dep':
                 log_data = Logs_Data['dep_i'].tolist()
-                (abs_val,avg_val) = reading(i,log_data)
+                (abs_val) = reading(i,log_data)
             elif param == 'temp':
                 log_data = Logs_Data['temp_i'].tolist()
-                (abs_val,avg_val) = reading(i,log_data)
+                (abs_val) = reading(i,log_data)
             elif param == 'hum':
                 log_data = Logs_Data['hum_i'].tolist()
-                (abs_val,avg_val) = reading(i,log_data)
+                (abs_val) = reading(i,log_data)
             elif param == 'light':
                 log_data = Logs_Data['light_i'].tolist()
-                (abs_val,avg_val) = reading(i,log_data)
+                (abs_val) = reading(i,log_data)
             else:
                 continue
-            sending_data = {'Parameter':param, 'abs_val':abs_val, 'avg_val':avg_val}
+            sending_data = {'Parameter':param, 'abs_val':abs_val}
             sending_data_encoded = pickle.dumps(sending_data)
             sending_data_enc = rsa.encrypt(sending_data_encoded, pub_act)
             conn.sendall(sending_data_enc)
-            print("Sent Data")
+            print("Sent Encrypted Data")
         conn.close()
         try:
             conn.sendall(b'ping')
             print('Still open')
         except:
-            print('Closed')
+            print('Connection Closed')
         
-        time.sleep(20)
+        time.sleep(2)
